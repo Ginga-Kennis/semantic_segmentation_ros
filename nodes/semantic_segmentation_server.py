777b21bd4a9e28d6609dde231f23a02f203b6273
Ginga Kennis <gingakennis123@icloud.com>
2024-04-10 16:18:57 +0900

    confirm evaluation

--- nodes/semantic_segmentation_server.py
+++ nodes/semantic_segmentation_server.py
@@ -1,23 +1,39 @@
 #!/usr/bin/env python3
 
-from pathlib import Path
 import rospy
+import cv_bridge
+import numpy as np
+from pathlib import Path
 from sensor_msgs.msg import Image
 
 from semantic_segmentation_ros.detection import SemanticSegmentation
 
 class SemanticSegmentationServer:
     def __init__(self):
-        self.image_topic = rospy.get_param("~camera")
+        self.load_parameters()
+        self.init_topics()
+        self.init_semantic_segmentation_server()
+        self.cv_bridge = cv_bridge.CvBridge()
+        rospy.loginfo("Semantic Segmentation Server is ready")
+    
+    def load_parameters(self):
+        self.color_topic = rospy.get_param("~color_topic")
         self.model_path = Path(rospy.get_param("~model_path"))
-        self.semantic_segmentation = SemanticSegmentation(self.model_path)
+        rospy.loginfo(self.color_topic)
         rospy.loginfo(self.model_path)
-        rospy.loginfo(self.image_topic)
 
-        self.subscriber = rospy.Subscriber(self.image_topic, Image, self.image_callback)
+    def init_topics(self):
+        rospy.Subscriber(self.color_topic, Image, self.sensor_cb)
 
-    def image_callback(self, msg):
-        rospy.loginfo("Received an image!")
+    def init_semantic_segmentation_server(self):
+        self.ss_server =  SemanticSegmentation(self.model_path)
+
+    def sensor_cb(self, msg):
+        img = self.cv_bridge.imgmsg_to_cv2(msg).astype(np.float32)
+        y_pred = self.ss_server.predict(img)
+        print(y_pred.shape)
+
+
 
 if __name__ == "__main__":
     rospy.init_node("semantic_segmentation_server")
--- src/semantic_segmentation_ros/detection.py
+++ src/semantic_segmentation_ros/detection.py
@@ -1,14 +1,16 @@
 import torch
-
+import numpy as np
 from semantic_segmentation_ros.networks import load_model
 
 class SemanticSegmentation:
     def __init__(self, model_path):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        print(model_path)
         self.model = load_model(model_path, self.device)
+        self.model.eval()
 
     def predict(self, img):
+        img = torch.from_numpy(img).permute(2, 0, 1).unsqueeze(0).to(self.device)
+        
         with torch.no_grad():
             y_pred = self.model(img)
         
