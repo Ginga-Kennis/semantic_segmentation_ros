1f34a2dc672204910fa33ecbe46d1becb92ffb96
Ginga Kennis <gingakennis123@icloud.com>
2024-04-24 17:25:16 +0900

    refactor dataset

--- src/semantic_segmentation_ros/dataset.py
+++ src/semantic_segmentation_ros/dataset.py
@@ -1,43 +1,65 @@
 import os
-import cv2
-import json
 import torch
 import numpy as np
 from torch.utils.data import Dataset
 import torchvision.transforms.v2 as transforms
 from torchvision import tv_tensors
 
-from semantic_segmentation_ros.utils import vis_img_mask
+from semantic_segmentation_ros.utils import get_image, get_labelme_mask, vis_img_mask
 
 class SegDataset(Dataset):
-    def __init__(self, path, labels, augmentations, is_train=True):
+    """A dataset class for semantic segmentation which handles data loading, augmentations, and preprocessing.
+
+    Inputs:
+        path (str) - Base path to the dataset.
+        labels (dict) - Class labels.
+        augmentations (dict) - Dictionary specifying augmentation parameters.
+        is_train (bool) - Specifies if the dataset is for training. Defaults to True.
+    """
+
+    def __init__(self, path: str, labels: list, augmentations: dict, is_train: bool = True):
         self.labels = labels
         self.img_path = os.path.join(path, "img")
         self.ann_path = os.path.join(path, "ann")
         self.imgs = list(sorted(os.listdir(self.img_path)))
+        self.anns = list(sorted(os.listdir(self.ann_path)))
 
         self.augment = augmentations["enable"]
         self.trans = build_transform(augmentations)
 
         self.is_train = is_train
 
-    def __len__(self):
+    def __len__(self) -> int:
+        """Returns the number of images in the dataset."""
         return len(self.imgs)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int) -> tuple:
+        """
+        Fetches the image and mask tensors by index.
+
+        Inputs: idx (int) - The index of the data item.
+        Outputs: tuple - Tuple containing the image (Tensor) and mask (Tensor).
+        """
         img_path = os.path.join(self.img_path, self.imgs[idx])
-        mask_path = os.path.join(self.ann_path, self.imgs[idx].replace('.png', '.json').replace('.jpg', '.json'))
+        mask_path = os.path.join(self.ann_path, self.anns[idx])
 
         img = torch.tensor(get_image(img_path), dtype=torch.float32)
-        mask = torch.tensor(get_mask(img, mask_path, self.labels), dtype=torch.float32)  # [num_classes, height, width]
+        mask = torch.tensor(get_labelme_mask(img, mask_path, self.labels), dtype=torch.float32)  # [num_classes, height, width]
 
         if self.is_train == True and self.augment == True:
             img, mask = self.trans(tv_tensors.Image(img), tv_tensors.Mask(mask))
+
         mask = add_background(mask)
 
         return img, mask
     
-def build_transform(augmentations):
+def build_transform(augmentations: dict) -> transforms.Compose:
+    """
+    Builds a composed torchvision transformation based on augmentation settings.
+
+    Inputs: augmentations (dict) - Augmentation settings as a dictionary.
+    Outputs: transforms.Compose - A torchvision Compose object containing all the transformations.
+    """
     transform_list = []
 
     if augmentations["affine"]["enable"]:
@@ -52,44 +74,12 @@ def build_transform(augmentations):
     trans = transforms.Compose(transform_list)
     return trans
     
-def add_background(mask):
+def add_background(mask: torch.Tensor) -> torch.Tensor:
+    """
+    Adds a background channel to the mask tensor.
+
+    Inputs: mask (torch.Tensor) - The original mask tensor without the background channel.
+    Outputs: torch.Tensor - Updated mask tensor with the background channel added.
+    """
     background = torch.all(mask == 0, dim=0, keepdim=True)
     return torch.cat((mask, background), dim=0)
-
-def get_image(img_path):
-    img = cv2.imread(img_path).astype(np.float32)
-    if img is None:
-        raise FileNotFoundError(f"Image not found: {img_path}")
-    return np.transpose(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), (2, 0, 1))
-     
-
-def get_mask(img, mask_path, labels):
-    with open(mask_path) as handle:
-        data = json.load(handle)
-    shape_dicts = data["shapes"]
-
-    channels = []
-
-    # class in mask
-    cls = [x['label'] for x in shape_dicts]
-    
-    # points of each class
-    poly = [np.array(x['points'], dtype=np.int32) for x in shape_dicts]
-
-    # dictionary where key:label, value:points
-    label2poly = dict(zip(cls, poly))
-
-    # define background
-    _, height, width = img.shape
-
-    for label in labels:
-        
-        blank = np.zeros(shape=(height, width), dtype=np.float32)
-        
-        if label in cls:
-            cv2.fillPoly(blank, [label2poly[label]], 1)
-            
-        channels.append(blank)
-
-    Y = np.stack(channels, axis=0)
-    return Y
--- src/semantic_segmentation_ros/utils.py
+++ src/semantic_segmentation_ros/utils.py
@@ -1,6 +1,45 @@
+import cv2
+import json
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
+
+def get_image(img_path):
+    img = cv2.imread(img_path).astype(np.float32)
+    if img is None:
+        raise FileNotFoundError(f"Image not found: {img_path}")
+    return np.transpose(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), (2, 0, 1))
+
+def get_labelme_mask(img, mask_path, labels):
+    with open(mask_path) as handle:
+        data = json.load(handle)
+    shape_dicts = data["shapes"]
+
+    channels = []
+
+    # class in mask
+    cls = [x['label'] for x in shape_dicts]
+    
+    # points of each class
+    poly = [np.array(x['points'], dtype=np.int32) for x in shape_dicts]
+
+    # dictionary where key:label, value:points
+    label2poly = dict(zip(cls, poly))
+
+    # define background
+    _, height, width = img.shape
+
+    for label in labels:
+        
+        blank = np.zeros(shape=(height, width), dtype=np.float32)
+        
+        if label in cls:
+            cv2.fillPoly(blank, [label2poly[label]], 1)
+            
+        channels.append(blank)
+
+    Y = np.stack(channels, axis=0)
+    return Y
 
 def vis_img_mask(img, mask):
     # 画像データを適切な形式に変換（CHW -> HWC）
